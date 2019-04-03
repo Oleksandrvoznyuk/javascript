803a58b0fdf3012b11d2246635dc16cc65199063
Oleksandr <voznyuk.o.2014@gmail.com>
2019-04-03 22:46:30 +0300

    Add If else and Swithc

--- js/Lesson_7.js
+++ js/Lesson_7.js
@@ -1,5 +1,35 @@
-if( 2*4 == 9 ){
-    console.log('Right');
-}else{
+// if( 2*4 == 9 ){
+//     console.log('Right');
+// }else{
+//     console.log('No');
+// }
+
+
+let num = 50;
+
+// if(num < 49){
+//     console.log('Not right');
+// }else if( num > 100){
+//     console.log('Too much');
+// }else{
+//     console.log('Right');
+// }
+
+// (num == 50) ? console.log('Yes') : console.log('No');
+
+switch (num){
+    case num < 49:
     console.log('No');
+    break;
+
+    case num > 100:
+    console.log('Noo');
+    break;
+
+    case 50:
+    console.log('Yess');
+    break;
+
+    default:
+    console.log('Something wrong');
 }
